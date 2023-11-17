ba03775d64570f1f6b9d76db1f6be7c7f35c7a39
xova14 <xovarus@gmail.com>
2023-11-17 22:49:30 +0300

    added support for pyvirtualdisplay

--- test_chromedriver.py
+++ test_chromedriver.py
@@ -2,10 +2,17 @@
 
 # Importing required dependencies
 import undetected_chromedriver as uc
+import time
+from pyvirtualdisplay import Display
+
+
+display = Display(visible=0, size=(1024, 768))
+display.start()
 
 def test():
 	print("Starting testing")
-	driver = uc.Chrome(headless=True,use_subprocess=False)
+	time.sleep(5)
+	driver = uc.Chrome(headless=False,use_subprocess=False)
 	driver.get('https://nowsecure.nl')
 	driver.save_screenshot('nowsecure.png')	
 	print("Ending testing")
